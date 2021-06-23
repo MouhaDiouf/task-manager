c2c6bf0909ab7fc1c23237c7bb77f49956d61f96
Mouhamadou <mouhamadoudioufsn@gmail.com>
2021-06-23 07:52:56 +0000

    create User model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,4 +2,16 @@ const mongoose = require('mongoose');
 mongoose.connect('mongdb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true, 
     useCreateIndex: true,
-} )
+}); 
+
+const User = mongoose.model('User', {
+name: {
+    type: String   
+}, 
+
+age: {
+    type: Number
+}
+});
+
+const john = new User({name: 'John', age: 27})
