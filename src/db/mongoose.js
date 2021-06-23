d3868f185b1b65405d5299649af89e4412206a70
Mouhamadou <mouhamadoudioufsn@gmail.com>
2021-06-23 08:49:48 +0000

    add email validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,17 +1,65 @@
-const mongoose = require('mongoose'); 
-mongoose.connect('mongdb://127.0.0.1:27017/task-manager-api', {
-    useNewUrlParser: true, 
-    useCreateIndex: true,
-}); 
+
+const mongoose = require("mongoose");
+const validator = require('validator'); 
+
+
+mongoose.connect("mongodb://localhost:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 const User = mongoose.model('User', {
 name: {
-    type: String   
-}, 
+    type: String, 
+    required: true
+},
 
 age: {
-    type: Number
+    type: Number, 
+    validate(value){
+        if(value < 0){
+            throw new Error('Age must be a postitive number')
+        }
+    },
+}, 
+   email: {
+       type: String,
+       required: true, 
+    validate(value){
+      if(!validator.isEmail(value)){
+          throw new Error('Email is not valid')
+      }
+    }
+   
 }
 });
 
-const john = new User({name: 'John', age: 27})
+// const Task = mongoose.model("Task", {
+//   description: {
+//     type: String,
+//   },
+
+//   completed: {
+//     type: Boolean,
+//   },
+// });
+// const task = new Task({description: 'First task of the db', completed: true})
+
+ const john = new User({
+     name: 'Mouha', 
+     email: 'mouha@'
+     
+    //  email: 'mouha@'
+ });
+
+
+
+john
+  .save()
+  .then(() => {
+    console.log(john);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
