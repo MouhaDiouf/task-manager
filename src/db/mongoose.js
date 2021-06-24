dc5b4c92bb16d9473e25c3c532280bc7b84b7f91
Mouhamadou <mouhamadoudioufsn@gmail.com>
2021-06-24 08:00:00 +0000

    Add validation for user

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,5 @@
-
 const mongoose = require("mongoose");
-const validator = require('validator'); 
-
+const validator = require("validator");
 
 mongoose.connect("mongodb://localhost:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -9,30 +7,44 @@ mongoose.connect("mongodb://localhost:27017/task-manager-api", {
   useUnifiedTopology: true,
 });
 
-const User = mongoose.model('User', {
-name: {
-    type: String, 
-    required: true
-},
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+  },
 
-age: {
-    type: Number, 
-    validate(value){
-        if(value < 0){
-            throw new Error('Age must be a postitive number')
-        }
-    },
-}, 
-   email: {
-       type: String,
-       required: true, 
-    validate(value){
-      if(!validator.isEmail(value)){
-          throw new Error('Email is not valid')
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a postitive number");
       }
-    }
-   
-}
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is not valid");
+      }
+    },
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minLength: 7,
+    trim: true,
+    validate(value) {
+      if (value.includes("password")) {
+        throw new Error('Password should not contain "password" ');
+      }
+    },
+  },
 });
 
 // const Task = mongoose.model("Task", {
@@ -46,14 +58,13 @@ age: {
 // });
 // const task = new Task({description: 'First task of the db', completed: true})
 
- const john = new User({
-     name: 'Mouha', 
-     email: 'mouha@'
-     
-    //  email: 'mouha@'
- });
+const john = new User({
+  name: "Mouha",
+  email: "mouha@example.com",
+  password: "passwordis1",
 
-
+  //  email: 'mouha@'
+});
 
 john
   .save()
