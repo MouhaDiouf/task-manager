ea03bc4c13eec285246ae7bb66e679298ef942ff
MouhaDiouf <mouhamadoudioufsn@gmail.com>
2021-06-29 07:20:27 +0000

    add timestamps to user

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("./task");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -48,6 +49,7 @@ const userSchema = new mongoose.Schema({
       },
     },
   ],
+  timestams: true,
 });
 
 // statics. is accessible on models
@@ -99,7 +101,13 @@ userSchema.pre("save", async function (next) {
   }
   next();
 });
+// delete user tasks when user is removed
 
+userSchema.pre("remove", async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
+  next();
+});
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
